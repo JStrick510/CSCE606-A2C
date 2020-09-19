d1dcf2dbdab37582e3c887117d2b87ebfc0e6974
Ahmed Gad <ahmed.f.gad@gmail.com>
2020-09-19 03:11:20 +0200

    Change log directory to "."

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2269,7 +2269,7 @@ class MaskRCNN():
         # Directory for training logs
 #        self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(
 #            self.config.NAME.lower(), now))
-        self.log_dir = "D:\\Object Detection\\Tutorial\\2. Training Mask R-CNN in TensorFlow 2.0\\code\\logs"
+        self.log_dir = "."
 
         # Path to save after each epoch. Include placeholders that get filled by Keras.
         self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
