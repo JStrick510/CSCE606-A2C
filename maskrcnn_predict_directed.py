29bbece451fb0c9a20fb4623f5be0743823160da
Ahmed Gad <ahmed.f.gad@gmail.com>
2021-01-19 23:29:07 -0500

    Directed Mask R-CNN
    Example to use the Directed R-CNN where the model returns the region proposals found by the ProposalLayer.

---
+++ maskrcnn_predict_directed.py
@@ -0,0 +1,53 @@
+import mrcnn_directed
+import mrcnn_directed.config
+import mrcnn_directed.model
+import mrcnn_directed.visualize
+import cv2
+import os
+
+# load the class label names from disk, one label per line
+# CLASS_NAMES = open("coco_labels.txt").read().strip().split("\n")
+
+CLASS_NAMES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+class SimpleConfig(mrcnn_directed.config.Config):
+    # Give the configuration a recognizable name
+    NAME = "coco_inference"
+    
+    # set the number of GPUs to use along with the number of images per GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+	# Number of classes = number of classes + 1 (+1 for the background). The background class is named BG
+    NUM_CLASSES = len(CLASS_NAMES)
+
+# Initialize the Mask R-CNN model for inference and then load the weights.
+# This step builds the Keras model architecture.
+model = mrcnn_directed.model.MaskRCNN(mode="inference", 
+                                      config=SimpleConfig(),
+                                      model_dir=os.getcwd())
+
+# Load the weights into the model.
+model.load_weights(filepath="mask_rcnn_coco.h5", 
+                   by_name=True)
+
+# load the input image, convert it from BGR to RGB channel
+image = cv2.imread("test.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Perform a forward pass of the network to obtain the results
+r = model.detect([image], verbose=0)
+
+# Get the results for the first image.
+r = r[0]
+
+r2 = r.copy()
+
+r2[:, 0] = r2[:, 0] * 291.0
+r2[:, 2] = r2[:, 2] * 291.0
+r2[:, 1] = r2[:, 1] * 445.0
+r2[:, 3] = r2[:, 3] * 445.0
+
+# Visualize the detected objects.
+mrcnn_directed.visualize.display_instances(image=image, 
+                                           boxes=r2)
