329abc18094f2ab1a0b087b2243387a2cf74275e
Ahmed Gad <ahmed.f.gad@gmail.com>
2021-05-10 15:36:28 -0400

    change requirements and edit transfer learning code

--- kangaroo-transfer-learning/kangaroo_training.py
+++ kangaroo-transfer-learning/kangaroo_training.py
@@ -8,60 +8,61 @@ import mrcnn.model
 
 class KangarooDataset(mrcnn.utils.Dataset):
 
-	def load_dataset(self, dataset_dir, is_train=True):
-		self.add_class("dataset", 1, "kangaroo")
+    def load_dataset(self, dataset_dir, is_train=True):
+        # Adds information (image ID, image path, and annotation file path) about each image in a dictionary.
+        self.add_class("dataset", 1, "kangaroo")
 
-		images_dir = dataset_dir + '/images/'
-		annotations_dir = dataset_dir + '/annots/'
+        images_dir = dataset_dir + '/images/'
+        annotations_dir = dataset_dir + '/annots/'
 
-		for filename in os.listdir(images_dir):
-			image_id = filename[:-4]
+        for filename in os.listdir(images_dir):
+            image_id = filename[:-4]
 
-			if is_train and int(image_id) >= 150:
-				continue
+            if is_train and int(image_id) >= 150:
+                continue
 
-			if not is_train and int(image_id) < 150:
-				continue
+            if not is_train and int(image_id) < 150:
+                continue
 
-			img_path = images_dir + filename
-			ann_path = annotations_dir + image_id + '.xml'
+            img_path = images_dir + filename
+            ann_path = annotations_dir + image_id + '.xml'
 
-			self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
+            self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
 
-	# extract bounding boxes from an annotation file
-	def extract_boxes(self, filename):
-		tree = xml.etree.ElementTree.parse(filename)
+    # Loads the binary masks for an image.
+    def load_mask(self, image_id):
+        info = self.image_info[image_id]
+        path = info['annotation']
+        boxes, w, h = self.extract_boxes(path)
+        masks = zeros([h, w, len(boxes)], dtype='uint8')
 
-		root = tree.getroot()
+        class_ids = list()
+        for i in range(len(boxes)):
+            box = boxes[i]
+            row_s, row_e = box[1], box[3]
+            col_s, col_e = box[0], box[2]
+            masks[row_s:row_e, col_s:col_e, i] = 1
+            class_ids.append(self.class_names.index('kangaroo'))
+        return masks, asarray(class_ids, dtype='int32')
 
-		boxes = list()
-		for box in root.findall('.//bndbox'):
-			xmin = int(box.find('xmin').text)
-			ymin = int(box.find('ymin').text)
-			xmax = int(box.find('xmax').text)
-			ymax = int(box.find('ymax').text)
-			coors = [xmin, ymin, xmax, ymax]
-			boxes.append(coors)
+    # A helper method to extract the bounding boxes from the annotation file
+    def extract_boxes(self, filename):
+        tree = xml.etree.ElementTree.parse(filename)
 
-		width = int(root.find('.//size/width').text)
-		height = int(root.find('.//size/height').text)
-		return boxes, width, height
+        root = tree.getroot()
 
-	# load the masks for an image
-	def load_mask(self, image_id):
-		info = self.image_info[image_id]
-		path = info['annotation']
-		boxes, w, h = self.extract_boxes(path)
-		masks = zeros([h, w, len(boxes)], dtype='uint8')
+        boxes = list()
+        for box in root.findall('.//bndbox'):
+            xmin = int(box.find('xmin').text)
+            ymin = int(box.find('ymin').text)
+            xmax = int(box.find('xmax').text)
+            ymax = int(box.find('ymax').text)
+            coors = [xmin, ymin, xmax, ymax]
+            boxes.append(coors)
 
-		class_ids = list()
-		for i in range(len(boxes)):
-			box = boxes[i]
-			row_s, row_e = box[1], box[3]
-			col_s, col_e = box[0], box[2]
-			masks[row_s:row_e, col_s:col_e, i] = 1
-			class_ids.append(self.class_names.index('kangaroo'))
-		return masks, asarray(class_ids, dtype='int32')
+        width = int(root.find('.//size/width').text)
+        height = int(root.find('.//size/height').text)
+        return boxes, width, height
 
 class KangarooConfig(mrcnn.config.Config):
     NAME = "kangaroo_cfg"
@@ -73,20 +74,20 @@ class KangarooConfig(mrcnn.config.Config):
 
     STEPS_PER_EPOCH = 131
 
-# prepare train set
-train_set = KangarooDataset()
-train_set.load_dataset(dataset_dir='kangaroo', is_train=True)
-train_set.prepare()
+# Train
+train_dataset = KangarooDataset()
+train_dataset.load_dataset(dataset_dir='kangaroo', is_train=True)
+train_dataset.prepare()
 
-# prepare test/val set
-valid_dataset = KangarooDataset()
-valid_dataset.load_dataset(dataset_dir='kangaroo', is_train=False)
-valid_dataset.prepare()
+# Validation
+validation_dataset = KangarooDataset()
+validation_dataset.load_dataset(dataset_dir='kangaroo', is_train=False)
+validation_dataset.prepare()
 
-# prepare config
+# Model Configuration
 kangaroo_config = KangarooConfig()
 
-# define the model
+# Build the Mask R-CNN Model Architecture
 model = mrcnn.model.MaskRCNN(mode='training', 
                              model_dir='./', 
                              config=kangaroo_config)
@@ -95,8 +96,8 @@ model.load_weights(filepath='mask_rcnn_coco.h5',
                    by_name=True, 
                    exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
 
-model.train(train_dataset=train_set, 
-            val_dataset=valid_dataset, 
+model.train(train_dataset=train_dataset, 
+            val_dataset=validation_dataset, 
             learning_rate=kangaroo_config.LEARNING_RATE, 
             epochs=1, 
             layers='heads')
